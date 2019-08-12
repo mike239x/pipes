360f871e43a1e13a295b835107d18720bd1387ea
Mike Lezhnin <mikhail.lezhnin@esrlabs.com>
2019-08-12 14:50:58 +0200

    Put workers into separate functions
    Workers (threads) are created by separate functions and hence can
    potentially be separated from the main.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,52 @@
 use std::thread;
 use std::sync;
-use std::sync::mpsc::RecvError;
+use std::sync::mpsc::{ Sender, Receiver, RecvError };
 
-trait Pipe<T> where T: Copy {
-    fn push(t: T);
-    fn pop() -> T;
+fn square_things(input : Receiver<f32>, output : Sender<f32>) -> thread::JoinHandle<()> {
+    let worker = thread::spawn(move || loop {
+        match input.recv() {
+            Ok(x) => {
+                output.send(x*x).unwrap();
+            }
+            Err(RecvError) => { break; }
+        }
+    });
+    return worker;
 }
 
+fn half_things(input : Receiver<f32>, output : Sender<f32>) -> thread::JoinHandle<()> {
+    let worker = thread::spawn(move || loop {
+        match input.recv() {
+            Ok(x) => {
+                output.send(x / 2f32).unwrap();
+            }
+            Err(RecvError) => { break; }
+        }
+    });
+    return worker;
+}
+
+fn add_things(input_a : Receiver<f32>, input_b : Receiver<f32>, output : Sender<f32>) -> thread::JoinHandle<()> {
+    let worker = thread::spawn(move || loop {
+        match input_a.recv() {
+            Ok(x) => {
+                match input_b.recv() {
+                    Ok(y) => {
+                        output.send(x+y).unwrap();
+                    }
+                    _ => break
+                }
+            }
+            _ => break
+        };
+    });
+    return worker;
+}
+
+
 fn main() {
-    let a = vec!(1,2,3);
-    let b = vec!(6,4,2);
+    let a = vec!(1f32,2f32,3f32);
+    let b = vec!(6f32,4f32,2f32);
     let mut c = vec!();
     // we what to get a^2 + b/2 using threads
     // w1 will do a^2
@@ -22,35 +59,9 @@ fn main() {
         let (sa2, ra2) = sync::mpsc::channel();
         let (sb, rb) = sync::mpsc::channel();
         let (sb2, rb2) = sync::mpsc::channel();
-        w1 = thread::spawn(move || loop {
-            match ra.recv() {
-                Ok(x) => {
-                    sa2.send(x*x).unwrap();
-                }
-                Err(RecvError) => { break; }
-            }
-        });
-        w2 = thread::spawn(move || loop {
-            match rb.recv() {
-                Ok(x) => {
-                    sb2.send(x/2).unwrap();
-                }
-                Err(RecvError) => { break; }
-            }
-        });
-        w3 = thread::spawn(move || loop {
-            match ra2.recv() {
-                Ok(x) => {
-                    match rb2.recv() {
-                        Ok(y) => {
-                            sc.send(x+y).unwrap();
-                        }
-                        _ => break
-                    }
-                }
-                _ => break
-            };
-        });
+        w1 = square_things(ra, sa2);
+        w2 = half_things(rb, sb2);
+        w3 = add_things(ra2, rb2, sc);
         for x in a {
             sa.send(x).unwrap();
         }
